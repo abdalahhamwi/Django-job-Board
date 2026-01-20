c5ee33f5a7700ef7c03be6f2b57f36f3cc4fc22b
abdala hamwi <abdalah09941@gmail.com>
2026-01-20 14:21:30 +0300

    update contact.html

--- project/settings.py
+++ project/settings.py
@@ -79,10 +79,9 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
