5352b7bd0518418dfc4ecf358b0db4893fb08f38
abdala hamwi <abdalah09941@gmail.com>
2026-01-29 19:48:52 +0300

    add accounts templates

--- project/urls.py
+++ project/urls.py
@@ -14,16 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path , include
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('blog/', include('blog.urls')),
-    path('jobs/', include('job.urls')),
-    path('contact/', include('contact.urls')),
-    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.   MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("", include("home.urls")),
+        path("blog/", include("blog.urls")),
+        path("jobs/", include("job.urls")),
+        path("contact/", include("contact.urls")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
